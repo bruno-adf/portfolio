be565b1588c1b4556753d8ec2bcd4c52b52dc628
bruno-adf <113475095+bruno-adf@users.noreply.github.com>
2023-05-16 00:05:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import './Style.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+
+      </header>
+      <main>
+        <section className='landing'>
+          
+        </section>
+        <section className='about'>
+          
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
